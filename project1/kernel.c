ec2c9a1295c46a10b0e43b7250e8bf80ec1e24c1
DougKeller <dwk24@zips.uakron.edu>
2016-02-15 21:15:14 -0500

    Readme

--- project1/kernel.c
+++ project1/kernel.c
@@ -1,3 +1,29 @@
+﻿/* ACADEMIC INTEGRITY PLEDGE                                              */
+/*                                                                        */
+/* - I have not used source code obtained from another student nor        */
+/*   any other unauthorized source, either modified or unmodified.        */
+/*                                                                        */
+/* - All source code and documentation used in my program is either       */
+/*   my original work or was derived by me from the source code           */
+/*   published in the textbook for this course or presented in            */
+/*   class.                                                               */
+/*                                                                        */
+/* - I have not discussed coding details about this project with          */
+/*   anyone other than my instructor. I understand that I may discuss     */
+/*   the concepts of this program with other students and that another    */
+/*   student may help me debug my program so long as neither of us        */
+/*   writes anything during the discussion or modifies any computer       */
+/*   file during the discussion.                                          */
+/*                                                                        */
+/* - I have violated neither the spirit nor letter of these restrictions. */
+/*                                                                        */
+/*                                                                        */
+/*                                                                        */
+/* Signed:___________Douglas Keller____________ Date:__2/15/2016__        */
+/*                                                                        */
+/*                                                                        */
+/* 3460:4/526 BlackDOS kernel, Version 1.02, Spring 2016.                 */
+
 void printString(char*);
 void readString(char*);
 void clearScreen(int, int);
@@ -17,15 +43,18 @@ void main() {
 
     interrupt(33, 12, 4, 11, 0);
 
-    interrupt(33, 0, "\r\n  oooooooooo.   oooo    oooo         oooooooooo.     .oooooo.    .oooooo..o\0", 0, 0); 
-    interrupt(33, 0, "\r\n  `888'   `Y8b  `888   .8P'          `888'   `Y8b   d8P'  `Y8b  d8P'    `Y8\0", 0, 0); 
-    interrupt(33, 0, "\r\n   888      888  888  d8'             888      888 888      888 Y88bo.     \0", 0, 0); 
-    interrupt(33, 0, "\r\n   888      888  88888[               888      888 888      888  `\"Y8888o. \0", 0, 0); 
-    interrupt(33, 0, "\r\n   888      888  888`88b.    8888888  888      888 888      888      `\"Y88b\0", 0, 0); 
-    interrupt(33, 0, "\r\n   888     d88'  888  `88b.           888     d88' `88b    d88' oo     .d8P\0", 0, 0); 
-    interrupt(33, 0, "\r\n  o888bood8P'   o888o  o888o         o888bood8P'    `Y8bood8P'  8""888888P' \0", 0, 0); 
-           
-    interrupt(33, 0, "\r\n", 0, 0);               
+    interrupt(33, 0, "\r\n  oooooooooo.   oooo    oooo         oooooooooo.     .oooooo.    .oooooo..o\0", 0, 0);
+    interrupt(33, 0, "\r\n  `888'   `Y8b  `888   .8P'          `888'   `Y8b   d8P'  `Y8b  d8P'    `Y8\0", 0, 0);
+    interrupt(33, 0, "\r\n   888      888  888  d8'             888      888 888      888 Y88bo.     \0", 0, 0);
+    interrupt(33, 0, "\r\n   888      888  88888[               888      888 888      888  `\"Y8888o. \0", 0, 0);
+    interrupt(33, 0, "\r\n   888      888  888`88b.    8888888  888      888 888      888      `\"Y88b\0", 0, 0);
+    interrupt(33, 0, "\r\n   888     d88'  888  `88b.           888     d88' `88b    d88' oo     .d8P\0", 0, 0);
+    interrupt(33, 0, "\r\n  o888bood8P'   o888o  o888o         o888bood8P'    `Y8bood8P'  8""888888P' \0", 0, 0);     
+    
+    interrupt(33, 0, "\r\n", 0, 0);
+    interrupt(33, 0, "\r\n  Author: Douglas Keller\0", 0, 0);
+    interrupt(33, 0, "\r\n", 0, 0);
+
     interrupt(33, 0, "\r\nEnter a line: \0", 0, 0);
     interrupt(33, 1, line, 0, 0);
     interrupt(33, 0, "\r\nYou typed: \0", 0, 0);
@@ -45,7 +74,7 @@ void main() {
 void printString(char* message) {
     int i = 0;
 
-    // Output chars from the message until null is met
+    /* Output chars from the message until null is met */
     while(message[i] != 0x0) {
         interrupt(16, 14 * 256 + message[i], 0, 0, 0);
         i++;
@@ -57,21 +86,21 @@ void readString(char* message) {
     char inputChar;
 
     while(index < lastIndex) {
-        // Read the next character
+        /* Read the next character */
         inputChar = interrupt(22, 0, 0, 0, 0);
 
         switch(inputChar) {
-            case 0x8: // Backspace
+            case 0x8: /* Backspace */
                 if(index > 0) {
                     index--;
                     message[index] = 0;
-                    // Clear the previously printed char from the terminal
+                    /* Clear the previously printed char from the terminal */
                     interrupt(16, 14 * 256 + inputChar, 0, 0, 0);
                     interrupt(16, 14 * 256 + ' ', 0, 0, 0);
                     interrupt(16, 14 * 256 + inputChar, 0, 0, 0);
                 }
                 break;
-            case 0xD: // Enter
+            case 0xD: /* Enter */
                 message[index] = 0x0;
                 return;
             default:
@@ -143,26 +172,26 @@ void readInt(int* address) {
     char inputChar;
 
     while(index < maxIndex) {
-        // Read the next character
+        /* Read the next character */
         inputChar = interrupt(22, 0, 0, 0, 0);
 
         switch(inputChar) {
-            case 0x8: // Backspace
+            case 0x8: /* Backspace */
                 if(index > -1) {
                     digits[index] = 0;
                     index--;
-                    // Clear the previously printed char from the terminal
+                    /* Clear the previously printed char from the terminal */
                     interrupt(16, 14 * 256 + inputChar, 0, 0, 0);
                     interrupt(16, 14 * 256 + ' ', 0, 0, 0);
                     interrupt(16, 14 * 256 + inputChar, 0, 0, 0);
                 }
                 break;
-            case 0xD: // Enter
-                // Set maxIndex to -1, which will terminate the loop
+            case 0xD: /* Enter */
+                /* Set maxIndex to -1, which will terminate the loop */
                 maxIndex = -1;
                 break;
             default:
-                // Verify input is an integer character
+                /* Verify input is an integer character */
                 if(inputChar >= 48 && inputChar < 58) {
                     index++;
                     digits[index] = inputChar;
@@ -171,7 +200,7 @@ void readInt(int* address) {
         }
     }
 
-    // Convert the int array to a single int
+    /* Convert the int array to a single int */
     while(index >= 0) {
         sum += (digits[index] - '0') * power;
         power *= 10;
